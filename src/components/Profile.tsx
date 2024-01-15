45ad61aa146a3c90a036c4a7d16b92db1204bf54
ParkJuneWoo <krca0220@gmail.com>
2024-01-15 22:28:17 +0900

    add profile context

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,27 +1,30 @@
+import AuthContext from "context/AuthContext";
 import { getAuth, signOut } from "firebase/auth";
 import { app } from "firebaseApp";
+import { useContext } from "react";
 import { toast } from "react-toastify";
 
-export default function Profile() {
+const onSignOut = async () => {
   const auth = getAuth(app);
 
-  const onSignOut = async () => {
-    try {
-      await signOut(auth);
-      toast.success("로그아웃 되었습니다.");
-    } catch (error: any) {
-      toast.error(error?.code);
-    }
-  };
+  try {
+    await signOut(auth);
+    toast.success("로그아웃 되었습니다.");
+  } catch (error: any) {
+    toast.error(error?.code);
+  }
+};
+
+export default function Profile() {
+  const { user } = useContext(AuthContext);
+
   return (
     <div className="profile__box">
       <div className="flex__box-lg">
         <div className="profile__image"></div>
         <div>
-          <div className="profile__email">{auth.currentUser?.email}</div>
-          <div className="profile__name">
-            {auth.currentUser?.displayName || "사용자"}
-          </div>
+          <div className="profile__email">{user?.email}</div>
+          <div className="profile__name">{user?.displayName || "사용자"}</div>
         </div>
       </div>
       <div role="presentation" className="profile__logout" onClick={onSignOut}>
