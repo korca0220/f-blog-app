8c3a9dcca98aa7a26f992abe3238f5511ac58f89
ParkJuneWoo <krca0220@gmail.com>
2024-01-15 20:53:56 +0900

    login

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,38 @@
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { app } from "firebaseApp";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+import Loader from "components/Loader";
 import Router from "./components/Router";
 
 function App() {
   const auth = getAuth(app);
 
+  // auth를 체크하기 전에 loader를 띄워주는 용도
+  const [init, setInit] = useState(false);
+
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
     !!auth?.currentUser
   );
 
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+
+      setInit(true);
+    });
+  }, [auth]);
+
   return (
     <>
       <ToastContainer />
-      <Router isAuthenticated={isAuthenticated} />
+      {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
     </>
   );
 }
