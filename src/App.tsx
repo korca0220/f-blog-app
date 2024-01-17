2e96a3482d8d7d02593636931e471dfa2ba9899f
ParkJuneWoo <krca0220@gmail.com>
2024-01-17 20:41:37 +0900

    dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { app } from "firebaseApp";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import Loader from "components/Loader";
+import { ThemeContext } from "context/ThemeContext";
 import Router from "./components/Router";
 
 function App() {
+  const themeContext = useContext(ThemeContext);
   const auth = getAuth(app);
 
   // auth를 체크하기 전에 loader를 띄워주는 용도
@@ -30,10 +32,10 @@ function App() {
   }, [auth]);
 
   return (
-    <>
+    <div className={themeContext.theme === "light" ? "white" : "dark"}>
       <ToastContainer />
       {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
-    </>
+    </div>
   );
 }
 
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,11 +8,10 @@ export default function Footer() {
 
   return (
     <footer>
-      <div>
-        <Link to={"/posts/new"}>글쓰기</Link>
-        <Link to={"/posts/"}>게시글</Link>
-        <Link to={"/profile"}>프로필</Link>
-      </div>
+      <Link to={"/posts/new"}>글쓰기</Link>
+      <Link to={"/posts/"}>게시글</Link>
+      <Link to={"/profile"}>프로필</Link>
+
       <div>
         {context.theme === "light" ? (
           <BsSun onClick={context.toggleMode} className="footer__theme-btn" />
